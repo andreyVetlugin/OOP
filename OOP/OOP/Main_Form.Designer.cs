7d353c730a6adf96218db72e5bfa743e34eec886
Сергей Тостницкий <sergeytrost@gmail.com>
2019-03-06 21:08:52 +0500

    second_table

--- OOP/OOP/Main_Form.Designer.cs
+++ OOP/OOP/Main_Form.Designer.cs
@@ -33,12 +33,16 @@
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button_recount = new System.Windows.Forms.Button();
+            this.second_dataGridView = new System.Windows.Forms.DataGridView();
             this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.button_recount = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.first_dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.second_dataGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // first_dataGridView
@@ -51,13 +55,10 @@
             this.Column1,
             this.Column2,
             this.Column3,
-            this.Column4,
-            this.Column5,
-            this.Column6,
-            this.Column7});
-            this.first_dataGridView.Location = new System.Drawing.Point(12, 12);
+            this.Column4});
+            this.first_dataGridView.Location = new System.Drawing.Point(12, 32);
             this.first_dataGridView.Name = "first_dataGridView";
-            this.first_dataGridView.Size = new System.Drawing.Size(1094, 146);
+            this.first_dataGridView.Size = new System.Drawing.Size(801, 133);
             this.first_dataGridView.TabIndex = 0;
             // 
             // Column1
@@ -82,35 +83,14 @@
             // Column4
             // 
             this.Column4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Column4.HeaderText = "Оборачиваемость ДЗ, \nдни";
+            this.Column4.HeaderText = "Оборачиваемость ДЗ,  дни";
             this.Column4.Name = "Column4";
             this.Column4.ReadOnly = true;
-            // 
-            // Column5
-            // 
-            this.Column5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Column5.HeaderText = "Балл по отрасли";
-            this.Column5.Name = "Column5";
-            this.Column5.ReadOnly = true;
-            // 
-            // Column6
-            // 
-            this.Column6.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Column6.HeaderText = "Сумма баллов";
-            this.Column6.Name = "Column6";
-            this.Column6.ReadOnly = true;
-            // 
-            // Column7
-            // 
-            this.Column7.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Column7.HeaderText = "Балл";
-            this.Column7.Name = "Column7";
-            this.Column7.ReadOnly = true;
             // 
             // button_recount
             // 
             this.button_recount.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button_recount.Location = new System.Drawing.Point(12, 164);
+            this.button_recount.Location = new System.Drawing.Point(12, 280);
             this.button_recount.Name = "button_recount";
             this.button_recount.Size = new System.Drawing.Size(120, 30);
             this.button_recount.TabIndex = 1;
@@ -118,45 +98,106 @@
             this.button_recount.UseVisualStyleBackColor = true;
             this.button_recount.Click += new System.EventHandler(this.button_recount_Click);
             // 
-            // button1
+            // second_dataGridView
             // 
-            this.button1.Location = new System.Drawing.Point(210, 236);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.second_dataGridView.AllowUserToAddRows = false;
+            this.second_dataGridView.AllowUserToDeleteRows = false;
+            this.second_dataGridView.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.second_dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.second_dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8});
+            this.second_dataGridView.Location = new System.Drawing.Point(12, 207);
+            this.second_dataGridView.Name = "second_dataGridView";
+            this.second_dataGridView.Size = new System.Drawing.Size(801, 45);
+            this.second_dataGridView.TabIndex = 3;
+            // 
+            // Column5
+            // 
+            this.Column5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column5.HeaderText = "План, тыс.руб";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column6.HeaderText = "Факт, тыс.руб";
+            this.Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            this.Column7.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column7.HeaderText = "выполнение, %";
+            this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
+            // 
+            // Column8
+            // 
+            this.Column8.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column8.HeaderText = "Балл";
+            this.Column8.Name = "Column8";
+            this.Column8.ReadOnly = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(243, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Оборачиваемость ДЗ по отраслям:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 187);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(360, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Выполнение плана по погашению просроченной ДЗ:";
             // 
             // Main_Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1118, 437);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.button_recount);
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(867, 334);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.second_dataGridView);
             this.Controls.Add(this.first_dataGridView);
+            this.Controls.Add(this.button_recount);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Main_Form";
             this.Text = "Лучший филиал";
             ((System.ComponentModel.ISupportInitialize)(this.first_dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.second_dataGridView)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView first_dataGridView;
+        private System.Windows.Forms.Button button_recount;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridView second_dataGridView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
-        private System.Windows.Forms.Button button_recount;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
     }
 }
 
--- OOP/OOP/Main_Form.cs
+++ OOP/OOP/Main_Form.cs
@@ -16,31 +16,35 @@ namespace OOP
         {
             InitializeComponent();
 
-            first_dataGridView.Rows.Add("Промышленность", "0", "0", "0", "0", "0", "0");
-            first_dataGridView.Rows.Add("Население", "0", "0", "0", "0", "0", "0");
-            first_dataGridView.Rows.Add("Бюджет", "0", "0", "0", "0", "0", "0");
-            first_dataGridView.Rows.Add("ОПП, ЖКХ и др.", "0", "0", "0", "0", "0", "0");
-            first_dataGridView.Rows.Add("Прочее", "0", "0", "0", "0", "0", "0");
-            //first_dataGridView.Rows[0].Cells[1].Value= "Промышленность";
+            first_dataGridView.Rows.Add("Промышленность", "0", "0", "0");
+            first_dataGridView.Rows.Add("Население", "0", "0", "0");
+            first_dataGridView.Rows.Add("Бюджет", "0", "0", "0");
+            first_dataGridView.Rows.Add("ОПП, ЖКХ и др.", "0", "0", "0");
+            first_dataGridView.Rows.Add("Прочее", "0", "0", "0");
+            second_dataGridView.Rows.Add("0", "0", "0", "0");
         }
 
         private void button_recount_Click(object sender, EventArgs e)
         {
+            int first_param, second_param;
             for (int i = 0; i < first_dataGridView.Rows.Count; i++)
             {
-                if (!int.TryParse(first_dataGridView[1, i].Value.ToString(), out int first_param))
-                    return;
-                if (!int.TryParse(first_dataGridView[2, i].Value.ToString(), out int second_param))
-                    return;
+                if (!int.TryParse(first_dataGridView[1, i].Value.ToString(), out first_param))
+                    continue;
+                if (!int.TryParse(first_dataGridView[2, i].Value.ToString(), out second_param))
+                    continue;
                 if (first_param <= 0 || second_param <= 0)
-                    return;
+                    continue;
                 first_dataGridView[3, i].Value = (double)first_param * 365 / second_param;
             }
-        }
 
-        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
+            if (!int.TryParse(second_dataGridView[0, 0].Value.ToString(), out first_param))
+                return;
+            if (!int.TryParse(second_dataGridView[1, 0].Value.ToString(), out second_param))
+                return;
+            if (first_param <= 0 || second_param <= 0)
+                return;
+            second_dataGridView[2, 0].Value = (double)first_param / second_param * 100;
         }
     }
 }

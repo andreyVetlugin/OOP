37fa6c2af81980ee69794b2a7ba3c1ebb0b6c69e
Сергей Тостницкий <sergeytrost@gmail.com>
2019-03-07 22:57:26 +0500

    getTableFillers edit

--- OOP/OOP/Main_Form.cs
+++ OOP/OOP/Main_Form.cs
@@ -41,17 +41,14 @@ namespace OOP
 
     public class TableManager
     {
-        private delegate void TableFiller(DataGridView table);
-        //private List<DataGridView> tables;
-        //private Dictionary<int, string> tablesNames;
+        private delegate void TableFiller();
         private Dictionary<DataGridView, TableFiller> tablesFillers = new Dictionary<DataGridView, TableFiller>();
-        //хранить места ошибок, чтобы потом их вывести + имена для описания () передавать не лист а dict с именами-таблицами
 
         public TableManager(DataGridView[] tables)
         {
-            var numericTableFillers = getTableFillers();
+            TableFiller[] TableFillers = getTableFillers(tables);
             for (int i = 0; i < tables.Length; i++)
-                tablesFillers[tables[i]] = numericTableFillers[i];
+                tablesFillers[tables[i]] = TableFillers[i];
         }
 
         public static void InitializeTables(DataGridView[] Tables)
@@ -71,88 +68,88 @@ namespace OOP
 
         public void FillTable(DataGridView table)
         {
-            tablesFillers[table](table);
+            tablesFillers[table]();
         }
         
-        private List<TableFiller> getTableFillers()//!!! заменить все проверки данных на функцию
+        private TableFiller[] getTableFillers(DataGridView[] tables)
         {
-            var tablesFillers = new List<TableFiller>();
-            // МОжно ли расчитывать на порядок с add?
-            //double\int где что 
-            tablesFillers.Add((DataGridView currentTable) =>
+            TableFiller[] tablesFillers = new TableFiller[tables.Length];
+            tablesFillers[0] = () =>
             {
                 int first_param, second_param;
-                for (int i = 0; i < currentTable.Rows.Count; i++)
+                for (int i = 0; i < tables[0].Rows.Count; i++)
                 {
-                    if (!int.TryParse(currentTable[1, i].Value.ToString(), out first_param))
+                    if (!int.TryParse(tables[0][1, i].Value.ToString(), out first_param))
                         continue;
-                    if (!int.TryParse(currentTable[2, i].Value.ToString(), out second_param))
+                    if (!int.TryParse(tables[0][2, i].Value.ToString(), out second_param))
                         continue;
                     if (first_param <= 0 || second_param <= 0)
                         continue;
-                    currentTable[3, i].Value = (double)first_param * 365 / second_param;
+                    tables[0][3, i].Value = (double)first_param * 365 / second_param;
                 }
-            });
+            };
 
-            tablesFillers.Add((DataGridView currentTable) =>
+            tablesFillers[1] = () =>
             {
                 int first_param, second_param;
-                if (!int.TryParse(currentTable[0, 0].Value.ToString(), out first_param))
+                if (!int.TryParse(tables[1][0, 0].Value.ToString(), out first_param))
                     return;
-                if (!int.TryParse(currentTable[1, 0].Value.ToString(), out second_param))
+                if (!int.TryParse(tables[1][1, 0].Value.ToString(), out second_param))
                     return;
                 if (first_param <= 0 || second_param <= 0)
                     return;
-                currentTable[2, 0].Value = (double)first_param / second_param * 100;
+                tables[1][2, 0].Value = (double)first_param / second_param * 100;
 
-                if ((double)currentTable[2, 0].Value >= 120)
-                    currentTable[3, 0].Value = 6;
-                else if ((double)currentTable[2, 0].Value >= 110)
-                    currentTable[3, 0].Value = 5;
-                else if ((double)currentTable[2, 0].Value >= 100)
-                    currentTable[3, 0].Value = 4;
-                else if ((double)currentTable[2, 0].Value >= 95)
-                    currentTable[3, 0].Value = 3;
-                else if ((double)currentTable[2, 0].Value >= 90)
-                    currentTable[3, 0].Value = 2;
-                else currentTable[3, 0].Value = 1;
+                if ((double)tables[1][2, 0].Value >= 120)
+                    tables[1][3, 0].Value = 6;
+                else if ((double)tables[1][2, 0].Value >= 110)
+                    tables[1][3, 0].Value = 5;
+                else if ((double)tables[1][2, 0].Value >= 100)
+                    tables[1][3, 0].Value = 4;
+                else if ((double)tables[1][2, 0].Value >= 95)
+                    tables[1][3, 0].Value = 3;
+                else if ((double)tables[1][2, 0].Value >= 90)
+                    tables[1][3, 0].Value = 2;
+                else tables[1][3, 0].Value = 1;
 
-            });
+            };
 
-            tablesFillers.Add((DataGridView currentTable) =>
+            tablesFillers[2] = () =>
             {
                 int first_param, second_param;
                 for (var i = 0; i < 2; i++)
                 {
-                    if (!int.TryParse(currentTable[1, i].Value.ToString(), out first_param))
+                    if (!int.TryParse(tables[2][1, i].Value.ToString(), out first_param))
                         return;
-                    if (!int.TryParse(currentTable[2, i].Value.ToString(), out second_param))
+                    if (!int.TryParse(tables[2][2, i].Value.ToString(), out second_param))
                         return;
                     if (first_param <= 0 || second_param <= 0)
                         return;
-                    currentTable[3, i].Value = (double)first_param / second_param;
+                    tables[2][3, i].Value = (double)first_param / second_param;
                 }
-            });
-            tablesFillers.Add((DataGridView currentTable) =>
+            };
+
+            tablesFillers[3] = () =>
             {                
                 int first_param, second_param,errorsCount;
                 for (var i = 0; i < 2; i++)
                 {
-                    if (!int.TryParse(currentTable[1, i].Value.ToString(), out first_param))
+                    if (!int.TryParse(tables[3][1, i].Value.ToString(), out first_param))
                         return;
-                    if (!int.TryParse(currentTable[2, i].Value.ToString(), out second_param))
+                    if (!int.TryParse(tables[3][2, i].Value.ToString(), out second_param))
                         return;
-                    if (!int.TryParse(currentTable[3, i].Value.ToString(), out errorsCount))
+                    if (!int.TryParse(tables[3][3, i].Value.ToString(), out errorsCount))
                         return;
                     if (first_param <= 0 || second_param <= 0)
                         return;
-                    if (errorsCount != 1)                    
-                        currentTable[4, i].Value = 0;
+                    if (errorsCount != 1)
+                        tables[3][4, i].Value = 0;
                     else
-                        currentTable[4, i].Value = (double)first_param / second_param * errorsCount;
+                        tables[3][4, i].Value = (double)first_param / second_param * errorsCount;
                 }
-            });
-            tablesFillers.Add((DataGridView currentTable) => { });
+            };
+
+            tablesFillers[4] = () => { };
             return tablesFillers;
         }
     }
